3de4b1d2cfc0769ebc1b097eb56e2ac10908d29f
richardfreelancing2021 <richard.freelancing2021@gmail.com>
2021-04-21 20:57:02 +0500

    master | adding data in job details

--- src/Containers/Home/JobDetail/index.tsx
+++ src/Containers/Home/JobDetail/index.tsx
@@ -1,52 +1,87 @@
 import React from "react";
 import styles from "./index.module.scss";
 import { StarOutlined, BellOutlined } from "@ant-design/icons";
-
 import { Button, Typography } from "antd";
-const JobApply = () => {
+import moment from "moment";
+const JobApply = (data: any) => {
   const { Title, Text } = Typography;
+  const {
+    nameOfJob,
+    location,
+    recruiterType,
+    currencySymbol,
+    salaryLowerLimit,
+    salaryUpperLimit,
+    datePosted,
+  } = data.data;
   return (
-    <div className={styles.jobApplyFieldsWrapper}>
-      <div className={styles.jobApplyFields}>
-        <Title level={4} className={styles.jobContetHeading}>
-          Job Title
-        </Title>
-        <Text className={styles.jobContetPara}>Job Title</Text>
-        <Title level={4} className={styles.jobContetHeading}>
-          Location
-        </Title>
-        <Text className={styles.jobContetPara}>Address, State, Country</Text>
-        <Title level={4} className={styles.jobContetHeading}>
-          Employer
-        </Title>
-        <Text className={styles.jobContetPara}>We are hiring</Text>
-        <Title level={4} className={styles.jobContetHeading}>
-          Industry
-        </Title>
-        <Text className={styles.jobContetPara}>healthcare</Text>
-        <Title level={4} className={styles.jobContetHeading}>
-          Rate
-        </Title>
-        <Text className={styles.jobContetPara}>$15-$25</Text>
-        <Title level={4} className={styles.jobContetHeading}>
-          Salary
-        </Title>
-        <Text className={styles.jobContetPara}>$40,000-$50,000</Text>
-        <Title level={4} className={styles.jobContetHeading}>
-          Date Posted
-        </Title>
-        <Text className={styles.jobContetPara}>2020/04/01</Text>
+    <>
+      {console.log(data.data)}
+      <div className={styles.jobApplyFieldsWrapper}>
+        <div className={styles.jobApplyFields}>
+          <Title level={4} className={styles.jobContetHeading}>
+            Job Title
+          </Title>
+          {console.log(nameOfJob)}
+          <Text className={styles.jobContetPara}>{nameOfJob}</Text>
+          <Title level={4} className={styles.jobContetHeading}>
+            Location
+          </Title>
+          <Text className={styles.jobContetPara}>{location}</Text>
+          <Title level={4} className={styles.jobContetHeading}>
+            Employer
+          </Title>
+          <Text className={styles.jobContetPara}>{recruiterType}</Text>
+          <Title level={4} className={styles.jobContetHeading}>
+            Industry
+          </Title>
+          <Text className={styles.jobContetPara}>healthcare</Text>
+          <Title level={4} className={styles.jobContetHeading}>
+            Rate
+          </Title>
+          <Text className={styles.jobContetPara}>$15-$25</Text>
+          <Title level={4} className={styles.jobContetHeading}>
+            Salary
+          </Title>
+          <Text className={styles.jobContetPara}>
+            {currencySymbol}
+            {salaryLowerLimit}-{currencySymbol}
+            {salaryUpperLimit}
+          </Text>
+          <Title level={4} className={styles.jobContetHeading}>
+            Date Posted
+          </Title>
+          <Text className={styles.jobContetPara}>
+            {moment(datePosted).format("MMM Do YYYY")}
+          </Text>
+        </div>
+        <Button
+          block
+          icon={<StarOutlined />}
+          size="large"
+          className={styles.buttonAlignmant}
+        >
+          Bookmark
+        </Button>{" "}
+        <Button
+          block
+          icon={<BellOutlined />}
+          size="large"
+          className={styles.buttonAlignmant}
+        >
+          Alert
+        </Button>
+        <Button
+          block
+          size="large"
+          className={styles.buttonAlignmant}
+          type="primary"
+        >
+          Apply
+        </Button>
       </div>
-      <Button block icon={<StarOutlined />} size="large" className={styles.buttonAlignmant}>
-        Bookmark
-      </Button>{" "}
-      <Button block icon={<BellOutlined />} size="large" className={styles.buttonAlignmant}>
-        Alert
-      </Button>
-      <Button block size="large" className={styles.buttonAlignmant} type="primary">
-        Apply
-      </Button>
-    </div>
+      )
+    </>
   );
 };
 

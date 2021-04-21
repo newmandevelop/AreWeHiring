186b7597e68c4a522499803d0e6627bded6df174
richardfreelancing2021 <richard.freelancing2021@gmail.com>
2021-04-21 20:55:54 +0500

    master | jobSearchById saga

---
+++ src/Containers/Home/saga.ts
@@ -0,0 +1,21 @@
+import { put, takeLatest, call } from "redux-saga/effects";
+import moment from "moment";
+import { ActionTypes, Actions } from "./actions";
+import { JobSearch } from "../../service/index";
+function* searchJobById(action: any) {
+  const { id } = action.payload;
+  try {
+    const res = yield call(JobSearch.jobSearchById, id);
+    if (res) {
+      yield put(Actions.jobByIdSuccess(res));
+    } else {
+      yield put(Actions.jobByIdFailure("Data Not Found"));
+    }
+  } catch (error) {
+    yield put(Actions.jobByIdFailure(error));
+  }
+}
+
+export default function* jobSearchSaga() {
+  yield takeLatest(ActionTypes.JOB_BY_ID_PROGRESS, searchJobById);
+}

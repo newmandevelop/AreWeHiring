a8e142b689ec920e22d8dced0fc5e97157867d27
richardfreelancing2021 <richard.freelancing2021@gmail.com>
2021-04-21 20:52:23 +0500

    master | combine jobSearch Reducer

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux'
 // import Auth, { IAuthState } from './Containers/Auth/reducers'
+import JobSearch,{IJobSearch} from './Containers/Home/reducers'
 
 export interface IRootState {
   // authState: IAuthState
+  jobSearch:IJobSearch
 }
 
 const rootReducer = combineReducers({
   // authState: Auth,
+  jobSearch:JobSearch
 })
 
 export default rootReducer

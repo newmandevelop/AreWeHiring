2387ad6a6473cf047857b6aee35ba644f95c67d1
richardfreelancing2021 <richard.freelancing2021@gmail.com>
2021-04-21 14:58:36 +0500

    master | remove UserProfile and DBSchema from reducers

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,12 +1,6 @@
 import { combineReducers } from 'redux'
 // import Auth, { IAuthState } from './Containers/Auth/reducers'
 
-interface UserProfile {
-  [name: string]: any
-}
-interface DBSchema {
-  [name: string]: any
-}
 export interface IRootState {
   // authState: IAuthState
 }

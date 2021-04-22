48b196d298caa4e3f1b498943ea4a2ece65ef274
richardfreelancing2021 <richard.freelancing2021@gmail.com>
2021-04-22 22:24:05 +0500

    master | add footer in layout

--- src/Containers/Layout/index.tsx
+++ src/Containers/Layout/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import CustomeHeader from "./CustomHeader";
 import { Layout } from "antd";
-const { Content } = Layout;
+const { Content, Footer } = Layout;
 const CustomLayout = (props: any) => {
   return (
     <Layout style={{ backgroundColor: "#f1f2f4" }} {...props}>
       <CustomeHeader {...props}></CustomeHeader>
-      <Content style={{ padding: "0px 5rem 3rem 5rem" }}>{props.children}</Content>
+      <Content style={{ padding: "0px 5rem 3rem 5rem" }}>
+        {props.children}
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+          backgroundColor: "#2b5468",
+          color: "white",
+        }}
+      >
+        We are Hiring ©2021
+      </Footer>
     </Layout>
   );
 };

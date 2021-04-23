4ce7abcf9f882c644af110a7215d2495102f67e3
richardfreelancing2021 <richard.freelancing2021@gmail.com>
2021-04-23 12:11:07 +0500

    job-api | add allJob reducer in root reducer

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,15 +1,17 @@
 import { combineReducers } from "redux";
 // import Auth, { IAuthState } from './Containers/Auth/reducers'
 import JobSearch, { IJobSearch } from "./Containers/JobOverview/reducers";
-
+import AllJobSearch, { IAllJobSearch } from "./Containers/AllJobs/reducers";
 export interface IRootState {
   // authState: IAuthState
   jobSearch: IJobSearch;
+  allJobsSearch: IAllJobSearch;
 }
 
 const rootReducer = combineReducers({
   // authState: Auth,
   jobSearch: JobSearch,
+  allJobsSearch: AllJobSearch,
 });
 
 export default rootReducer;

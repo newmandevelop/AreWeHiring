219e9d71b4b576bb55b141976fc84258fe8fbb9d
richardfreelancing2021 <richard.freelancing2021@gmail.com>
2021-04-23 12:31:09 +0500

    job-api | export  allJobs fetch function

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,8 +1,9 @@
-import { login } from './login'
-import {jobSearchById} from './jobSearch'
+import { login } from "./login";
+import { jobSearchById, allJobsSearch } from "./jobSearch";
 export const loginCheck = {
   login,
-}
-export const JobSearch={
+};
+export const JobSearch = {
   jobSearchById,
-}
+  allJobsSearch,
+};

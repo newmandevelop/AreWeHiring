b8c56880243f94650da80ae0b808edc762fc663e
richardfreelancing2021 <richard.freelancing2021@gmail.com>
2021-04-23 12:13:07 +0500

    job-api | AllJobs actions and constants

---
+++ src/Containers/AllJobs/actions.ts
@@ -0,0 +1,46 @@
+export enum ActionTypes {
+  ALL_JOBS_PROGRESS = "auth/ALL_JOBS_PROGRESS",
+  ALL_JOBS_SUCCESS = "auth/ALL_JOBS_SUCCESS",
+  ALL_JOBS_FAILURE = "auth/ALL_JOBS_FAILURE",
+}
+
+export interface AllJobsProgress {
+  type: ActionTypes.ALL_JOBS_PROGRESS;
+}
+
+export interface AllJobsSuccess {
+  type: ActionTypes.ALL_JOBS_SUCCESS;
+  payload: any;
+}
+
+export interface AllJobsFailure {
+  type: ActionTypes.ALL_JOBS_FAILURE;
+  payload: any;
+}
+
+function allJobsProgress(): AllJobsProgress {
+  return {
+    type: ActionTypes.ALL_JOBS_PROGRESS,
+  };
+}
+function allJobsSuccess(payload: any): AllJobsSuccess {
+  return {
+    type: ActionTypes.ALL_JOBS_SUCCESS,
+    payload,
+  };
+}
+
+function allJobsFailure(payload: any): AllJobsFailure {
+  return {
+    type: ActionTypes.ALL_JOBS_FAILURE,
+    payload,
+  };
+}
+
+export type Action = AllJobsProgress | AllJobsFailure | AllJobsSuccess;
+
+export const Actions = {
+  allJobsProgress,
+  allJobsSuccess,
+  allJobsFailure,
+};

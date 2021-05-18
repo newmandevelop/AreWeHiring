c4c8566ef99eaca8fd6c99caa9a3f7509abbdd67
richardfreelancing2021 <richard.freelancing2021@gmail.com>
2021-05-18 10:24:43 +0500

    find-job | review box

--- src/Pages/Home/Reviews/index.tsx
+++ src/Pages/Home/Reviews/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import styles from './index.module.scss';
-import { Tooltip } from 'antd';
 const Reviews = () => {
   return (
-    <div>
-      <Tooltip
-        title="I have already heard back about the internship I applied through Are We Hiring, that’s the fastest
-job reply I’ve ever gotten and it’s so much better than waiting weeks to hear back."
-      >
-        <span>Mouse over Here</span>
-      </Tooltip>
+    <div className={styles.reviewWrapper}>
+      <div className={styles.reviewBox}>
+        <div className={styles.review}>
+          <p>
+            I have already heard back about the internship I applied through Are
+            We Hiring, that’s the fastest job reply I’ve ever gotten and it’s so
+            much better than waiting weeks to hear back.
+          </p>
+        </div>
+      </div>
+
+      {/* <div className={styles.authorBox}>
+        <img
+          className={styles.authorImage}
+          src="https://www.arewehiring.com/wp-content/uploads/2015/10/John-Smith-780x430.jpg"
+          alt=""
+        />
+        <h4 className={styles.authorName}>
+          {' '}
+          <span></span>
+        </h4>
+      </div> */}
     </div>
   );
 };

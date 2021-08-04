8ae6b47d133b8e7927bce9bb4111e77152bdee3b
richardfreelancing2021 <richard.freelancing2021@gmail.com>
2021-08-04 21:46:31 +0500

    small-diff-tasks | candidate dummy data replaced with actual data

--- src/Pages/SearchCandidate/index.tsx
+++ src/Pages/SearchCandidate/index.tsx
@@ -61,38 +61,38 @@ export default function SearchCandidate() {
     candidateSearchFailure,
     candidateSearchErrorMessage,
   ]);
-  const candidateDataDummy = [
-    {
-      content: {
-        firstName: "Jackson",
-        lastName: "Smith",
-        minimumRate: "100",
-        professionalTitle: 'Developer',
-        location: 'United States',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget eleifend neque. Integer cursus nisi non laoreet interdum. Aenean in ex id nibh convallis aliquam eget mattis orci. Aliquam finibus semper rhoncus. Nulla facilisi. Suspendisse tristique egestas est, laoreet luctus ipsum feugiat ac. Quisque gravida blandit maximus. Nulla id lectus lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;'
-      }
-    },
-    {
-      content: {
-        firstName: "Harris",
-        lastName: "Edwards",
-        minimumRate: "80",
-        professionalTitle: 'Bussiness Manager',
-        location: 'United States',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget eleifend neque. Integer cursus nisi non laoreet interdum. Aenean in ex id nibh convallis aliquam eget mattis orci. Aliquam finibus semper rhoncus. Nulla facilisi. Suspendisse tristique egestas est, laoreet luctus ipsum feugiat ac. Quisque gravida blandit maximus. Nulla id lectus lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;'
-      }
-    },
-    {
-      content: {
-        firstName: "Liam",
-        lastName: "Cole",
-        minimumRate: "85",
-        professionalTitle: 'Senior Executive',
-        location: 'United States',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget eleifend neque. Integer cursus nisi non laoreet interdum. Aenean in ex id nibh convallis aliquam eget mattis orci. Aliquam finibus semper rhoncus. Nulla facilisi. Suspendisse tristique egestas est, laoreet luctus ipsum feugiat ac. Quisque gravida blandit maximus. Nulla id lectus lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;'
-      }
-    }
-  ]
+  // const candidateDataDummy = [
+  //   {
+  //     content: {
+  //       firstName: "Jackson",
+  //       lastName: "Smith",
+  //       minimumRate: "100",
+  //       professionalTitle: 'Developer',
+  //       location: 'United States',
+  //       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget eleifend neque. Integer cursus nisi non laoreet interdum. Aenean in ex id nibh convallis aliquam eget mattis orci. Aliquam finibus semper rhoncus. Nulla facilisi. Suspendisse tristique egestas est, laoreet luctus ipsum feugiat ac. Quisque gravida blandit maximus. Nulla id lectus lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;'
+  //     }
+  //   },
+  //   {
+  //     content: {
+  //       firstName: "Harris",
+  //       lastName: "Edwards",
+  //       minimumRate: "80",
+  //       professionalTitle: 'Bussiness Manager',
+  //       location: 'United States',
+  //       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget eleifend neque. Integer cursus nisi non laoreet interdum. Aenean in ex id nibh convallis aliquam eget mattis orci. Aliquam finibus semper rhoncus. Nulla facilisi. Suspendisse tristique egestas est, laoreet luctus ipsum feugiat ac. Quisque gravida blandit maximus. Nulla id lectus lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;'
+  //     }
+  //   },
+  //   {
+  //     content: {
+  //       firstName: "Liam",
+  //       lastName: "Cole",
+  //       minimumRate: "85",
+  //       professionalTitle: 'Senior Executive',
+  //       location: 'United States',
+  //       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget eleifend neque. Integer cursus nisi non laoreet interdum. Aenean in ex id nibh convallis aliquam eget mattis orci. Aliquam finibus semper rhoncus. Nulla facilisi. Suspendisse tristique egestas est, laoreet luctus ipsum feugiat ac. Quisque gravida blandit maximus. Nulla id lectus lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;'
+  //     }
+  //   }
+  // ]
   return (
     <Row style={{ margin: '1rem' }}>
       <Col span={6} style={{ backgroundColor: '' }}>
@@ -214,7 +214,7 @@ export default function SearchCandidate() {
             }}
           />}
           {candidateData &&
-            candidateDataDummy.map((candidate: any) => {
+            candidateData.map((candidate: any) => {
               return (
                 <div className={styles.description}>
                   <div style={{ padding: '1rem' }}>
